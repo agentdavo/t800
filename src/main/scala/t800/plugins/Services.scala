5d174315f9c9a1a6919f74516d78ee1aa3085b07
David Smith <44442387+agentdavo@users.noreply.github.com>
2025-06-13 13:11:51 +0100

    [M-5] timer control

--- src/main/scala/t800/plugins/Services.scala
+++ src/main/scala/t800/plugins/Services.scala
@@ -39,6 +39,18 @@ trait TimerSrv {
 
   /** Request to set the timers to a new value. */
   def set(value: UInt): Unit
+
+  /** Resume the high priority counter. */
+  def enableHi(): Unit
+
+  /** Resume the low priority counter. */
+  def enableLo(): Unit
+
+  /** Halt the high priority counter. */
+  def disableHi(): Unit
+
+  /** Halt the low priority counter. */
+  def disableLo(): Unit
 }
 
 trait InstrBusSrv {
--- src/main/scala/t800/plugins/TimerPlugin.scala
+++ src/main/scala/t800/plugins/TimerPlugin.scala
@@ -2,6 +2,7 @@ package t800.plugins
 
 import spinal.core._
 import spinal.lib._
+import spinal.core.sim._
 import t800.TConsts
 
 /** Simple high/low priority timers. High increments every cycle; low every 64 cycles. */
@@ -11,6 +12,8 @@ class TimerPlugin extends FiberPlugin {
   private var loCnt: UInt = null
   private var loadReq: Bool = null
   private var loadVal: UInt = null
+  private var hiEn: Bool = null
+  private var loEn: Bool = null
 
   override def setup(): Unit = {
     hiTimer = Reg(UInt(TConsts.WordBits bits)) init (0)
@@ -18,6 +21,12 @@ class TimerPlugin extends FiberPlugin {
     loCnt = Reg(UInt(6 bits)) init (0)
     loadReq = Reg(Bool()) init (False)
     loadVal = Reg(UInt(TConsts.WordBits bits)) init (0)
+    hiEn = Reg(Bool()) init (True)
+    loEn = Reg(Bool()) init (True)
+    hiTimer.simPublic()
+    loTimer.simPublic()
+    hiEn.simPublic()
+    loEn.simPublic()
     addService(new TimerSrv {
       override def hi: UInt = hiTimer
       override def lo: UInt = loTimer
@@ -25,6 +34,11 @@ class TimerPlugin extends FiberPlugin {
         loadReq := True
         loadVal := value
       }
+
+      override def enableHi(): Unit = hiEn #= true
+      override def enableLo(): Unit = loEn #= true
+      override def disableHi(): Unit = hiEn #= false
+      override def disableLo(): Unit = loEn #= false
     })
   }
 
@@ -34,10 +48,14 @@ class TimerPlugin extends FiberPlugin {
       loTimer := loadVal
       loCnt := 0
     } otherwise {
-      hiTimer := hiTimer + 1
-      loCnt := loCnt + 1
-      when(loCnt === 0) {
-        loTimer := loTimer + 1
+      when(hiEn) {
+        hiTimer := hiTimer + 1
+      }
+      when(loEn) {
+        loCnt := loCnt + 1
+        when(loCnt === 0) {
+          loTimer := loTimer + 1
+        }
       }
     }
     when(loadReq) { loadReq := False }
