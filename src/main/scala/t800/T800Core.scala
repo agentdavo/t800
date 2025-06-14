287619a2ad5d477e8365433a55a8d3d5861fb45e
David Smith <44442387+agentdavo@users.noreply.github.com>
2025-06-14 19:10:05 +0100

    Update T800Core.scala

--- src/main/scala/t800/T800Core.scala
+++ src/main/scala/t800/T800Core.scala
@@ -3,10 +3,10 @@ package t800
 import spinal.core._
 import spinal.lib.misc.database.Database
 import spinal.lib.misc.plugin.{PluginHost, FiberPlugin}
+import spinal.lib.bus.bmb.{Bmb, BmbParameter, BmbAccessParameter}
 import t800.plugins._
 
 object T800 {
-
   /** Create a database pre-loaded with defaults from [[Global]]. */
   def defaultDatabase(): Database = {
     val db = new Database
@@ -25,24 +25,35 @@ object T800 {
     db
   }
 
-  /** Standard plugin stack used by [[T800Core]] and [[TopVerilog]]. */
-  def defaultPlugins(): Seq[FiberPlugin] =
-    Seq(
-      new StackPlugin,
-      new PipelinePlugin,
-      new MainCachePlugin,
-      new WorkspaceCachePlugin,
-      new PmiPlugin,
-      new FetchPlugin,
-      new ChannelPlugin,
-      new GrouperPlugin,
-      new PrimaryInstrPlugin,
-      new SecondaryInstrPlugin,
-      new FpuPlugin,
-      new SchedulerPlugin,
-      new TimerPlugin,
-      new PipelineBuilderPlugin
+  /** Parameters for the 128-bit system bus. */
+  val systemBusParam = BmbParameter(
+    access = BmbAccessParameter(
+      addressWidth = Global.ADDR_BITS,
+      dataWidth = 128, // 128-bit wide system bus
+      lengthWidth = 4, // Supports up to 16-byte bursts
+      sourceWidth = 4, // Supports multiple masters (e.g., CPU, VCP)
+      contextWidth = 0
     )
+  )
+
+  /** Standard plugin stack used by [[T800Core]] and [[TopVerilog]], integrated with system bus. */
+  def defaultPlugins(): Seq[FiberPlugin] = Seq(
+    new StackPlugin,
+    new PipelinePlugin,
+    new MainCachePlugin,
+    new WorkspaceCachePlugin,
+    new PmiPlugin,
+    new FetchPlugin,
+    new ChannelPlugin,
+    new GrouperPlugin,
+    new PrimaryInstrPlugin,
+    new SecondaryInstrPlugin,
+    new FpuPlugin,
+    new SchedulerPlugin,
+    new TimerPlugin,
+    new PipelineBuilderPlugin,
+    new MemoryManagementPlugin // Added to replace MemoryPlugin
+  )
 }
 
 class T800(
@@ -50,9 +61,20 @@ class T800(
   plugins: Seq[FiberPlugin],
   database: Database = T800.defaultDatabase()
 ) extends Component {
+  val systemBus = master(Bmb(T800.systemBusParam))
+
   Database(database).on {
     host.asHostOf(plugins)
     plugins.foreach(_.awaitBuild())
+    // Connect plugins to system bus (to be implemented in each plugin)
+    plugins.foreach { plugin =>
+      plugin match {
+        case cache: MainCachePlugin => cache.connectToSystemBus(systemBus)
+        case workspace: WorkspaceCachePlugin => workspace.connectToSystemBus(systemBus)
+        case pmi: PmiPlugin => pmi.connectToSystemBus(systemBus)
+        case _ => // Other plugins may not need direct connection
+      }
+    }
   }
 }
 
