d242e23c66dcf94b82d4af405f1e842df84fb6d4
David Smith <44442387+agentdavo@users.noreply.github.com>
2025-06-15 17:25:45 +0100

    Update Multiplier.scala

--- src/main/scala/t800/plugins/fpu/Multiplier.scala
+++ src/main/scala/t800/plugins/fpu/Multiplier.scala
@@ -8,7 +8,7 @@ class FpuMultiplier extends Area {
   val io = new Bundle {
     val op1 = in Bits(64 bits)
     val op2 = in Bits(64 bits)
-    val isAbs = in Bool() // fpabs
+    val isAbs = in Bool()
     val roundingMode = in Bits(2 bits)
     val result = out Bits(64 bits)
     val cycles = out UInt(2 bits)
@@ -17,6 +17,8 @@ class FpuMultiplier extends Area {
   // Parse IEEE-754 operands
   val op1Parsed = parseIeee754(io.op1)
   val op2Parsed = parseIeee754(io.op2)
+  val op1Afix = AFix(op1Parsed.mantissa.asUInt, 52 bit, 0 exp)
+  val op2Afix = AFix(op2Parsed.mantissa.asUInt, 52 bit, 0 exp)
 
   when(io.isAbs) {
     io.result := packIeee754(False, op1Parsed.exponent, op1Parsed.mantissa)
@@ -25,17 +27,19 @@ class FpuMultiplier extends Area {
     // Multiplication
     val productSign = op1Parsed.sign ^ op2Parsed.sign
     val productExponent = op1Parsed.exponent + op2Parsed.exponent - 1023
-    val productMantissa = op1Parsed.mantissa * op2Parsed.mantissa
+    val productMantissa = (op1Afix * op2Afix).sat(2 pow 52 - 1, 0)
 
-    // Parallel rounding/post-normalization
-    val p = productMantissa
-    val pPlusOne = p + 1
-    val pPlusTwo = p + 2
-    val pShifted = p >> 1
-    val roundedMantissa = roundIeee754(p, io.roundingMode)
-    val finalExponent = productExponent + (p(104) ? 1 | 0)
+    // Rounding
+    val roundType = io.roundingMode.mux(
+      0 -> RoundType.ROUNDTOEVEN,
+      1 -> RoundType.FLOORTOZERO,
+      2 -> RoundType.CEIL,
+      3 -> RoundType.FLOOR
+    )
+    val roundedMantissa = productMantissa.round(0, roundType)
+    val finalExponent = productExponent + (productMantissa.raw(104) ? 1 | 0)
 
-    io.result := packIeee754(productSign, finalExponent, roundedMantissa)
-    io.cycles := 3 // 2/3 cycles single/double
+    io.result := packIeee754(productSign, finalExponent, roundedMantissa.raw)
+    io.cycles := 3
   }
 }
