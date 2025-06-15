3d0216c0ef3b9aac2ee232ce4c40692638cca49e
David Smith <44442387+agentdavo@users.noreply.github.com>
2025-06-15 14:13:29 +0100

    Create Service.scala

---
+++ src/main/scala/t800/plugins/mmu/Service.scala
@@ -0,0 +1,31 @@
+package t800.plugins.mmu
+
+import spinal.core._
+import spinal.lib._
+import t800.Global
+
+case class TrapHandlerSrv() extends Bundle {
+  val trapAddr = Bits(Global.ADDR_BITS bits)
+  val trapType = Bits(4 bits)
+  val trapEnable = Bool()
+  val trapHandlerAddr = Bits(Global.ADDR_BITS bits)
+  def setTrap(addr: Bits, typ: Bits): Unit = {
+    trapAddr := addr
+    trapType := typ
+    trapEnable := True
+  }
+  def clearTrap(): Unit = trapEnable := False
+}
+
+trait ConfigAccessSrv {
+  def addr: Bits
+  def data: Bits
+  def writeEnable: Bool
+  def isValid: Bool
+  def read(addr: Bits, width: Int): Bits
+  def write(addr: Bits, data: Bits, width: Int): Unit
+}
+
+trait AddressTranslationSrv {
+  def translate(addr: Bits): Bits
+}
