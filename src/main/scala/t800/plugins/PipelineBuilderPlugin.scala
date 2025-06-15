b1025d970d34076bf1b32c4066a8627f0d1fe6fc
David Smith <44442387+agentdavo@users.noreply.github.com>
2025-06-15 13:16:03 +0100

    Update PipelineBuilderPlugin.scala

--- src/main/scala/t800/plugins/PipelineBuilderPlugin.scala
+++ src/main/scala/t800/plugins/PipelineBuilderPlugin.scala
@@ -1,25 +1,45 @@
 package t800.plugins
 
 import spinal.core._
-import spinal.core.Area
+import spinal.lib.misc.plugin.{PluginHost, FiberPlugin}
 import spinal.core.fiber.Retainer
-import spinal.lib.misc.plugin._
-import spinal.lib.misc.pipeline
+import t800.{Global, T800}
 
-/** Collect all [[PipelineService]] implementations and build their links. */
-
-/** The function of this plugin is to collect all the pipeline parts defined by the other's
-  * PipelineService plugins and then invoke the spinal.lib.misc.pipeline.builder on them in order to
-  * finalize the pipelines hardware.
-  */
 class PipelineBuilderPlugin extends FiberPlugin {
-  val version = "PipelineBuilderPlugin v0.1"
-  val elaborationLock = Retainer()
-  val logic = during build new Area {
+  val version = "PipelineBuilderPlugin v0.3"
+  private val retain = Retainer()
+
+  during setup new Area {
+    println(s"[${this.getDisplayName()}] setup start")
     report(L"Initializing $version")
-    elaborationLock.await()
-    val chunks = host.list[PipelineService]
-    val links = chunks.flatMap(_.getLinks())
-    pipeline.Builder(links)
+    retain()
+    println(s"[${this.getDisplayName()}] setup end")
+  }
+
+  during build new Area {
+    println(s"[${this.getDisplayName()}] build start")
+    retain.await()
+    implicit val h: PluginHost = host
+    val pipe = Plugin[PipelineSrv]
+    val fetch = Plugin[FetchPlugin]
+    val grouper = try Plugin[GrouperPlugin] catch { case _: Exception => null } // Optional GrouperPlugin
+    val primary = Plugin[PrimaryInstrPlugin]
+
+    // Configure pipeline links
+    val links = if (grouper != null) {
+      // Grouped mode: Fetch -> Grouper -> PrimaryInstr
+      Seq(
+        StageLink(fetch.pipe.fetch.down, grouper.pipe.decode.up),
+        StageLink(grouper.out.down, primary.pipe.decode.up)
+      )
+    } else {
+      // Single-issue mode: Fetch -> PrimaryInstr
+      Seq(
+        StageLink(fetch.pipe.fetch.down, primary.pipe.decode.up)
+      )
+    }
+
+    pipe.addLinks(links)
+    println(s"[${this.getDisplayName()}] build end")
   }
 }
