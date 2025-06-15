5fd28ce3ed4ff46d808bd2c58b61f008ad514d39
David Smith <44442387+agentdavo@users.noreply.github.com>
2025-06-15 17:26:18 +0100

    Update DivRoot.scala

--- src/main/scala/t800/plugins/fpu/DivRoot.scala
+++ src/main/scala/t800/plugins/fpu/DivRoot.scala
@@ -6,53 +6,58 @@ import t800.plugins.fpu.Utils._
 
 class FpuDivRoot extends Area {
   val io = new Bundle {
-    val op1 = in Bits(64 bits) // Dividend or radicand
-    val op2 = in Bits(64 bits) // Divisor
+    val op1 = in Bits(64 bits)
+    val op2 = in Bits(64 bits)
     val isSqrt = in Bool()
-    val isRem = in Bool() // fprem
-    val isT805Step = in Bool() // fpusqrtstep, fpremstep
-    val isT805First = in Bool() // fpusqrtfirst, fpremfirst
-    val isT805Last = in Bool() // fpusqrtlast
+    val isRem = in Bool()
+    val isT805Step = in Bool()
+    val isT805First = in Bool()
+    val isT805Last = in Bool()
     val roundingMode = in Bits(2 bits)
     val result = out Bits(64 bits)
     val cycles = out UInt(10 bits)
+    val t805State = out Bits(64 bits)
   }
 
   // Parse IEEE-754 operands
   val op1Parsed = parseIeee754(io.op1)
   val op2Parsed = parseIeee754(io.op2)
+  val op1Afix = AFix(op1Parsed.mantissa.asUInt, 52 bit, 0 exp)
+  val op2Afix = AFix(op2Parsed.mantissa.asUInt, 52 bit, 0 exp)
 
   // SRT state
-  val quotient = Reg(Bits(64 bits)) init 0
-  val remainder = Reg(Bits(64 bits)) init op1Parsed.mantissa
-  val divisor = io.isSqrt ? quotient | op2Parsed.mantissa
+  val quotient = Reg(AFix(UQ(56 bit, 0 bit))) init 0
+  val remainder = Reg(AFix(UQ(56 bit, 0 bit))) init op1Afix
+  val divisor = Mux(io.isSqrt, quotient, op2Afix)
   val iteration = Reg(UInt(10 bits)) init 0
-  val maxIterations = Mux(io.isRem, 529, 15) // Double-precision
+  val maxIterations = Mux(io.isRem, 529, 15)
 
   // Microcode state
   when(io.isT805First) {
-    // Initialize T805 state
     quotient := 0
-    remainder := op1Parsed.mantissa
+    remainder := op1Afix
     iteration := 0
+    io.t805State := quotient.raw
   } elsewhen(io.isT805Step) {
-    // Perform one SRT step
-    // Placeholder
+    // SRT step (placeholder)
     iteration := iteration + 1
   } elsewhen(io.isT805Last && io.isSqrt) {
-    // Finalize square root
-    io.result := packIeee754(op1Parsed.sign, op1Parsed.exponent, quotient)
+    io.result := packIeee754(op1Parsed.sign, op1Parsed.exponent, quotient.raw)
     io.cycles := 15
   } otherwise {
-    // Standard SRT
     when(iteration < maxIterations) {
-      // Inspect 3 digits of borrow-save remainder
-      // Update quotient/remainder
+      // SRT step (placeholder)
       iteration := iteration + 1
     }
+    val roundType = io.roundingMode.mux(
+      0 -> RoundType.ROUNDTOEVEN,
+      1 -> RoundType.FLOORTOZERO,
+      2 -> RoundType.CEIL,
+      3 -> RoundType.FLOOR
+    )
     io.result := Mux(io.isRem,
-      remainder,
-      packIeee754(op1Parsed.sign, op1Parsed.exponent, quotient)
+      packIeee754(op1Parsed.sign, op1Parsed.exponent, remainder.round(0, roundType).raw),
+      packIeee754(op1Parsed.sign, op1Parsed.exponent, quotient.round(0, roundType).raw)
     )
     io.cycles := maxIterations
   }
