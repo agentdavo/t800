ba5c223a9df47953306f2f41de96b7382e652cba
David Smith <44442387+agentdavo@users.noreply.github.com>
2025-06-15 13:47:33 +0100

    Update Service.scala

--- src/main/scala/t800/plugins/cache/Service.scala
+++ src/main/scala/t800/plugins/cache/Service.scala
@@ -2,41 +2,51 @@ package t800.plugins.cache
 
 import spinal.core._
 import spinal.lib._
-
-case class WorkspaceCacheAccessSrv() extends Bundle {
-  val addrA = Bits(32 bits)
-  val addrB = Bits(32 bits)
-  val dataOutA = Bits(32 bits)
-  val dataOutB = Bits(32 bits)
-  val writeAddr = Bits(32 bits)
-  val writeData = Bits(32 bits)
-  val writeEnable = Bool()
-  val isHitA = Bool()
-  val isHitB = Bool()
-}
-
-case class MainCacheAccessSrv() extends Bundle {
-  val addr = Bits(32 bits)
-  val dataOut = Bits(128 bits)
-  val isHit = Bool()
-  val writeEnable = Bool()
-  val writeData = Bits(128 bits)
-}
-trait MainCacheSrv { def read(addr: Bits): Bits; def write(addr: Bits, data: Bits): Unit }
-trait WorkspaceCacheSrv { def write(addr: Bits, data: Bits): Unit; def fetch(addr: Bits): Bits }
+import t800.Global
 
 case class CacheReq() extends Bundle {
-  val addr  = Bits(32 bits)
-  val data  = Bits(32 bits)
-  val write = Bool()
+  val address = UInt(Global.ADDR_BITS bits)
+  val data = Bits(Global.WORD_BITS bits)
+  val isWrite = Bool()
 }
 
 case class CacheRsp() extends Bundle {
-  val data = Bits(32 bits)
-  val hit  = Bool()
+  val data = Bits(Global.WORD_BITS bits)
+  val valid = Bool()
+}
+
+case class WorkspaceCacheAccessSrv() extends Bundle {
+  val addrA = Bits(Global.ADDR_BITS bits)
+  val addrB = Bits(Global.ADDR_BITS bits)
+  val writeAddr = Bits(Global.ADDR_BITS bits)
+  val writeData = Bits(Global.WORD_BITS bits)
+  val writeEnable = Bool()
+  val isHitA = Bool()
+  val isHitB = Bool()
+  val dataOutA = Bits(Global.WORD_BITS bits)
+  val dataOutB = Bits(Global.WORD_BITS bits)
+}
+
+case class MainCacheAccessSrv() extends Bundle {
+  val addr = Bits(Global.ADDR_BITS bits)
+  val dataOut = Bits(128 bits) // 128-bit cache line
+  val isHit = Bool()
+  val writeEnable = Bool()
+  val writeData = Bits(128 bits)
 }
 
 trait CacheAccessSrv {
   def req: Flow[CacheReq]
   def rsp: Flow[CacheRsp]
+}
+
+trait MainCacheSrv {
+  def read(addr: UInt): Bits
+  def write(addr: UInt, data: Bits): Unit
+}
+
+trait WorkspaceCacheSrv {
+  def read(addr: UInt): Bits
+  def write(addr: UInt, data: Bits): Unit
+  def bus: Bmb // BMB interface for workspace cache
 }
