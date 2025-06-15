fb2a7f5cfbef8591bddff59bde50dd682b0fcfcb
David Smith <44442387+agentdavo@users.noreply.github.com>
2025-06-15 12:25:28 +0100

    Update Service.scala

--- src/main/scala/t800/plugins/fetch/Service.scala
+++ src/main/scala/t800/plugins/fetch/Service.scala
@@ -2,9 +2,10 @@ package t800.plugins.fetch
 
 import spinal.core._
 import spinal.lib._
+import t800.Global
 
-/** Service for instruction fetch, integrated with MainCachePlugin. */
+/** Service interface for instruction fetch, integrated with MainCachePlugin. */
 trait InstrFetchSrv {
-  def cmd: Flow[t800.MemReadCmd]  // BMB-based read command
-  def rsp: Flow[Bits]             // Response from MainCachePlugin
+  def cmd: Flow[Global.MemReadCmd]  // BMB-based read command
+  def rsp: Flow[Bits]               // Response with opcodes
 }
