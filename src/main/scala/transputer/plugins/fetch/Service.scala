db07fafb660d5cc6beadfb21f63877aff515b037
David Smith <44442387+agentdavo@users.noreply.github.com>
2025-06-22 18:37:15 +0100

    fix FetchPlugin service wiring

--- src/main/scala/transputer/plugins/fetch/Service.scala
+++ src/main/scala/transputer/plugins/fetch/Service.scala
@@ -2,10 +2,10 @@ package transputer.plugins.fetch
 
 import spinal.core._
 import spinal.lib._
-import transputer.Global
+import transputer.{Global, MemReadCmd}
 
 /** Service interface for instruction fetch, integrated with MainCachePlugin. */
 trait InstrFetchService {
-  def cmd: Flow[Global.MemReadCmd] // BMB-based read command
+  def cmd: Flow[MemReadCmd] // BMB-based read command
   def rsp: Flow[Bits] // Response with opcodes
 }
